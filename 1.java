a5ea5eab3df678dc926b9a9686831fed7a5571af
Antonio Junior <antonio.jr004@gmail.com>
2016-02-27 12:02:14 -0300

    nova versao

--- 1.java
+++ 1.java
@@ -2,7 +2,7 @@ public class teste{
 	public static void main (String[]args){
 	int i=16;
 	int j=2;
-	int media = (16+2)/2;
+	int media = (i+j)/2;
 	System.out.println("Aluno com média: "+media);
 	}
 }
